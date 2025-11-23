dab23fcdfae9d0bafab06cf69790ad911dbadbd9
Lava <wereop123@gmail.com>
2025-11-23 20:13:52 +0800

    4th Java-Homework-fix2
    add try logic or my computer show error = =
    that's annoying

--- hw_4nd_calendar.java
+++ hw_4nd_calendar.java
@@ -29,42 +29,42 @@ public class hw_4nd_calendar {
     }
     public static void main(String[] args) {
         Set<Integer> evenMonth = new HashSet<>(Set.of(2, 4, 6, 9, 11));
-        Scanner scn = new Scanner(System.in);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-        LocalDate date = LocalDate.parse(scn.nextLine(), formatter);
-        DayOfWeek dayOfWeek = date.getDayOfWeek();
-        System.out.println(dayOfWeek);
-        System.out.printf("S  M  T  W  T  F  S\n");
-        int month = date.getMonthValue();
-        int maxDate;
-        if(evenMonth.contains(month)) {
-            if(month == 2) {
-                maxDate = checkLeapYear(date.getYear()) ? 29 : 28;
-            } else {
-                maxDate = 30;
-            }
-        } else {
-            maxDate = 31;
-        }
-        int count = 1;
-        int shiftDate = date.getDayOfWeek().getValue() - (date.getDayOfMonth() % 7);
-        shiftDate = shiftDate < -1 ? shiftDate + 8 : shiftDate + 1;
-        int[] calendar = new int[maxDate + shiftDate];
-        for (int i = shiftDate; i < maxDate + shiftDate; i++) { //計算初始偏移天數後輸出月歷
-            calendar[i] = count;
-            count++;
-        }
-        List<List<Integer>> output = separateDate(calendar);
-        for (int index = 0; index < output.size(); index++) { //迴圈輸出(計算n禮拜)
-            for (int i = 0; i < output.get(index).size(); i++) { //迴圈輸出(計算n天)
-                if(output.get(index).get(i) != 0) {
-                    System.out.printf("%-2d ", output.get(index).get(i));
+        try (Scanner scn = new Scanner(System.in)) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+            LocalDate date = LocalDate.parse(scn.nextLine(), formatter);
+            DayOfWeek dayOfWeek = date.getDayOfWeek();
+            System.out.println(dayOfWeek);
+            System.out.printf("S  M  T  W  T  F  S\n");
+            int month = date.getMonthValue();
+            int maxDate;
+            if(evenMonth.contains(month)) {
+                if(month == 2) {
+                    maxDate = checkLeapYear(date.getYear()) ? 29 : 28;
                 } else {
-                    System.out.printf("   ");
+                    maxDate = 30;
                 }
+            } else {
+                maxDate = 31;
             }
-            System.out.printf("\n");
+            int count = 1;
+            int shiftDate = date.getDayOfWeek().getValue() - (date.getDayOfMonth() % 7);
+            shiftDate = shiftDate < -1 ? shiftDate + 8 : shiftDate + 1;
+            int[] calendar = new int[maxDate + shiftDate];
+            for (int i = shiftDate; i < maxDate + shiftDate; i++) { //計算初始偏移天數後輸出月歷
+                calendar[i] = count;
+                count++;
+            }
+            List<List<Integer>> output = separateDate(calendar);
+            for (int index = 0; index < output.size(); index++) { //迴圈輸出(計算n禮拜)
+                for (int i = 0; i < output.get(index).size(); i++) { //迴圈輸出(計算n天)
+                    if(output.get(index).get(i) != 0) {
+                        System.out.printf("%-2d ", output.get(index).get(i));
+                    } else {
+                        System.out.printf("   ");
+                    }
+                }
+                System.out.printf("\n");
+            }
         }
-        scn.close();
     }
 }
